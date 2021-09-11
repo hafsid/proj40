b51a4ce4357c5e82fc2b1ae17e6b74e72d104018
hafsid <50413142+hafsid@users.noreply.github.com>
2021-09-11 20:54:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+ 
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   
+}
